519f1d0663d175f326ffefcb698981b0d4f13f65
Gustavo Cristian Minuci de Almeida <e324146@fec.unicamp.br>
2024-01-10 11:36:29 -0300

    time delta calculation added for the search algorithm, printing the array is now optional

--- binary-search.c
+++ binary-search.c
@@ -1,7 +1,8 @@
 #include<stdlib.h>
 #include<stdio.h>
+#include<time.h>
 
-//Maximum number possible in the array
+// Maximum number possible in the array
 #define NUM_SIZE 36267
 
 void print_array(int array[], int array_length) {
@@ -9,6 +10,25 @@ void print_array(int array[], int array_length) {
         printf("%d ", array[i]);
     }
     printf("\n\n");
+}
+
+int check_arr_conditional(char cond) {
+    if(cond == 'y') {
+        return 1;
+    } else {
+        if(cond != 'n') {
+            printf("Invalid character, 'n' considered instead\n");
+        }
+        return -1;
+    }
+}
+
+void calculate_time(struct timespec end, struct timespec start) {
+    double time_delta = (end.tv_sec - start.tv_sec) + (end.tv_nsec - start.tv_nsec) / 1e9;;
+
+    printf("Start time: %f seconds\n", start);
+    printf("End time: %f seconds\n", end);
+    printf("Time delta: %f seconds\n", time_delta);
 }
 
 void selection_sort(int array[], int array_length) {
@@ -24,18 +44,30 @@ void selection_sort(int array[], int array_length) {
 }
 
 int binary_search(int array[], int array_length, int target) {
-    int low = 0;
+    int low = 0; 
+    int iterations = 0;
     int high = array_length - 1;
-
     while(low <= high) {
         int mid = low + (high - low) / 2;
+        iterations++;
 
         if(array[mid] == target) {
+            printf("Iterations: %d\n", iterations);
             return mid;
         } else if(array[mid] < target) {
             low = mid + 1;
         } else {
-            high = mid - 1;
+            high = mid - 1; 
+        }
+    }
+    printf("Iterations: %d\n", iterations);
+    return -1;
+}
+
+int linear_search(int array[], int array_length, int target) {
+    for(int i = 0; i < array_length; i++) {
+        if(array[i] == target) {
+            return i;
         }
     }
     return -1;
@@ -44,8 +76,10 @@ int binary_search(int array[], int array_length, int target) {
 int main() {
     int target;
     int array_length;
+    char arr_conditional;
 
-    //Creates an array dinamically based on user input for its length
+    printf("start clock: %ld\n", clock());
+    // Creates an array dinamically based on user input for its length
     printf("Enter a size for the array: ");
     scanf("%d", &array_length);
     int *array = malloc(array_length * sizeof(int));
@@ -53,35 +87,43 @@ int main() {
         fprintf(stderr, "Array memory allocation failed\n");
         return EXIT_FAILURE;
     }
+    printf("Do you want to print the array (This will seriously impact performance) y/n: ");
+    scanf(" %c", &arr_conditional);
+    printf("\n");
 
-    //Seeds rand
+    // Seeds rand
     srand((unsigned int)time(NULL));
-    //Populates the array with random numbers from 0 to NUM_SIZE
+    // Populates the array with random numbers from 0 to NUM_SIZE
     for(int i = 0; i < array_length; i++) {
         array[i] = rand() % NUM_SIZE;
-        printf("%d ", array[i]);
+        if(check_arr_conditional(arr_conditional) == 1) {
+            printf("%d ", array[i]);
+        }
+    }
+
+    selection_sort(array, array_length);
+    
+    if(check_arr_conditional(arr_conditional) == 1) {
+        printf("\n\nSorted array: ");
+        print_array(array, array_length);
     }
     printf("\n");
 
     printf("\nEnter a number to be found in the array: ");
-    scanf("%d", &target);
-
-    //printf("\nOriginal array: ");
-    //print_array(array, array_length);
-
-    selection_sort(array, array_length);
-
-    printf("Sorted array: ");
-    print_array(array, array_length);
-
+    scanf(" %d", &target);
+    // Record the start time
+    struct timespec search_start, search_end;
+    clock_gettime(CLOCK_MONOTONIC, &search_start);
     int res = binary_search(array, array_length, target);
-    //If the number is not found, the function returns -1
+    // Record the end time
+    clock_gettime(CLOCK_MONOTONIC, &search_end);
+    calculate_time(search_end, search_start);
+    // If the number is not found, the function returns -1
     if(res >= 0) {
-        printf("Number %d found at index %d", target, res);
+        printf("Number %d found at index %d\n", target, res);
     } else {
-        printf("Number not found");
+        printf("Number not found\n");
     }
-
     free(array);
     return EXIT_SUCCESS;
 }
